b41d97a38b7902e660ad4e7e2178e768c09982a8
Tim <timjonsson03@hotmail.com>
2024-01-30 12:28:53 +0100

    Data size

--- src/index.js
+++ src/index.js
@@ -6,6 +6,27 @@ const config = require("../config.json");
 const fs = require("fs");
 const db = require("./db/index");
 const { kill } = require("process");
+const MongoClient = require('mongodb').MongoClient;
+
+require("dotenv").config();
+
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.flnzudr.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(url);
+
+let stats;
+
+async function run() {
+  try {
+    await client.connect();
+    const db = await client.db("josh").command({ dbStats: 1});
+    stats = db.dataSize;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+run()
 
 require("./db/index");
 
@@ -19,8 +40,15 @@ async function getKillers() {
 }
 const killers = getKillers();
 
+function bytesToSize(bytes) {
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  if (bytes === 0) return '0 Byte';
+  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+  return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+}
+
 app.get('/', async (req, res) => {
-  res.render(path.join(__dirname, "public/main.ejs"), { amount: (await killers).length });
+  res.render(path.join(__dirname, "public/main.ejs"), { amount: (await killers).length, size: bytesToSize(stats)});
 });
 
 app.get("/rawstats", (req, res) => res.sendFile(path.join(__dirname, "public/rawstats.ejs")));
