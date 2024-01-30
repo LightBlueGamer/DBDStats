778495134026432927585df48843a2bba33935f2
Tim <timjonsson03@hotmail.com>
2024-01-30 11:56:49 +0100

    Changed to EJS

--- src/index.js
+++ src/index.js
@@ -5,20 +5,26 @@ const api = require("./api/index");
 const config = require("../config.json");
 const fs = require("fs");
 const db = require("./db/index");
+const { kill } = require("process");
 
 require("./db/index");
+
+app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + "/public"));
 app.use("/api", api);
 
 const indexTemplate = fs.readFileSync(path.join(__dirname, "public/main.html"), "utf-8");
 
-//async function getKillers() {
-  //  const killers = Array.from(await db.get("killers"));
-//}
-//getKillers();
+async function getKillers() {
+  return Array.from(await db.get("killers"));
+}
+const killers = getKillers();
 
-app.get("/", (req, res) => res.send(indexTemplate/*.replace("{amount}", killers.length)*/));
+app.get('/', async (req, res) => {
+  res.render(path.join(__dirname, "public/main.html"), { amount: (await killers).length });
+});
+
 app.get("/rawstats", (req, res) => res.sendFile(path.join(__dirname, "public/rawstats.html")));
 app.get("/addgame", (req, res) => res.sendFile(path.join(__dirname, "public/addgame.html")));
 app.get("/cperks", (req, res) => res.sendFile(path.join(__dirname, "public/commonperks.html")));
